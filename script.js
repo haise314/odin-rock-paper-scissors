3392b4012dcb83a92a0980c0bdb1351b727155a6
haise314 <yudsdanielpenaranda@gmail.com>
2026-02-12 04:35:59 +0800

    Add playGame and playRound functions
    Finished the main game loop from the function playGame()
    Added the scoring algorithm by comparing every possible scenario
    Added console logs for each round and tallying for the final score

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function getHumanChoice () {
     
     // If the human input is a String, convert it to a number
     if (typeof humanChoice === 'string' ) {
-        // Convert string to upper case for case sensitivity
+        // Convert string to upper case for case insensitivity
         humanChoice = humanChoice.toUpperCase();
         if (humanChoice === "ROCK") {
             return 1;
@@ -40,5 +40,58 @@ function getHumanChoice () {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound (humanChoice, computerChoice){
+    // compare the humanChoice and computerChoice
+    // check who wins
+    // update the scores
+    // Rock 1 Paper 2 Scissors 3
+    if (humanChoice == computerChoice) {
+        return "draw";
+    } else if (humanChoice === 1 && computerChoice === 2){
+        return "computer";
+    } else if (humanChoice === 1 && computerChoice === 3) {
+        return "human";
+    } else if (humanChoice === 2 && computerChoice === 3) {
+        return "computer";
+    } else if (humanChoice === 2 && computerChoice === 1) {
+        return "human";
+    } else if (humanChoice === 3 && computerChoice === 1) {
+        return "computer";
+    } else if (humanChoice === 3 && computerChoice === 2) {
+        return "human";
+    } else {
+        console.log("round selection winner error");
+    }
+}
+
+function playGame () {
+    // main looping function of the game
+    // decide who wins or game over conditions
+    // who wins in 5 rounds, best of three
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for ( let i = 0; i < 5; i++ ){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        let result = playRound (humanSelection, computerSelection);
+        console.log("Round "+ (i + 1) + " Winner: " + result);
+        if (result === "human") {
+            humanScore += 1;
+        } else if (result === "computer") {
+            computerScore += 1;
+        } else {
+            // draw scenario, nothing changes, idk.
+        }
+    }
+    if (humanScore > computerScore){
+        alert ("You win!");
+        console.log ("Your Score: " + humanScore + "| " + "Computer Score: " + computerScore);
+    } else {
+        alert ("You lost!");
+        console.log ("Your Score: " + humanScore + "| " + "Computer Score: " + computerScore);
+    }
+}
+
+playGame();
