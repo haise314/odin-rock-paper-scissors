d15bb3a956d97fc3b38a699cb1b252ab8c970dfc
haise314 <yudsdanielpenaranda@gmail.com>
2026-02-14 21:16:55 +0800

    Added basic UI interfance to the rps-ui

--- script.js
+++ script.js
@@ -2,42 +2,38 @@ const rock = 1;
 const paper = 2;
 const scissors = 3;
 
-// Generate computer choice for rock, paper, scissors
-// Get user input by typing either a number associated or word itself
-// Compare the two inputs
-// output the winner of the game
-
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     return computerChoice;
 }
 
+// Refactor
 function getHumanChoice () {
-    let humanChoice = prompt("1. Rock 2. Paper 3. Scissors? \n You can use numbers or words.");
+    // let humanChoice = prompt("1. Rock 2. Paper 3. Scissors? \n You can use numbers or words.");
     
-    // If the human input is a String, convert it to a number
-    if (typeof humanChoice === 'string' ) {
-        // Convert string to upper case for case insensitivity
-        humanChoice = humanChoice.toUpperCase();
-        if (humanChoice === "ROCK") {
-            return 1;
-        } else if (humanChoice === "PAPER") {
-            return 2;
-        } else if (humanChoice === "SCISSORS"){    
-            return 3;
-        } else if (humanChoice === "1") {
-            return Number(humanChoice);
-        } else if (humanChoice === "2") {
-            return Number(humanChoice);
-        } else if (humanChoice === "3") {
-            return Number(humanChoice);
-        }
-        else {
-            alert("Invalid input");
-        }
-    } else {
-        alert("Invalid Input");
-    }
+    // // If the human input is a String, convert it to a number
+    // if (typeof humanChoice === 'string' ) {
+    //     // Convert string to upper case for case insensitivity
+    //     humanChoice = humanChoice.toUpperCase();
+    //     if (humanChoice === "ROCK") {
+    //         return 1;
+    //     } else if (humanChoice === "PAPER") {
+    //         return 2;
+    //     } else if (humanChoice === "SCISSORS"){    
+    //         return 3;
+    //     } else if (humanChoice === "1") {
+    //         return Number(humanChoice);
+    //     } else if (humanChoice === "2") {
+    //         return Number(humanChoice);
+    //     } else if (humanChoice === "3") {
+    //         return Number(humanChoice);
+    //     }
+    //     else {
+    //         alert("Invalid input");
+    //     }
+    // } else {
+    //     alert("Invalid Input");
+    // }
 }
 
 function playRound (humanChoice, computerChoice){
@@ -94,4 +90,4 @@ function playGame () {
     }
 }
 
-playGame();
+// playGame();
